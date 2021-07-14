db987a8aedb85f8dce2231499a60bcc69949b33e
Kinron <3323502972@qq.com>
2021-07-14 17:44:27 +0800

    +

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,224 @@
 <template>
   <div class="home">
-    <nav class="navbar navbar-default" role="navigation">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse"
-            data-target="#example-navbar-collapse">
-          <span class="sr-only">切换导航</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">菜鸟教程</a>
+    <div class="container">
+      <div class="clearfix row">
+        <div class="col-md-12 column">
+          <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
+            </div>
+            
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active">
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="#">Action</a>
+                    </li>
+                    <li>
+                      <a href="#">Another action</a>
+                    </li>
+                    <li>
+                      <a href="#">Something else here</a>
+                    </li>
+                    <li class="divider">
+                    </li>
+                    <li>
+                      <a href="#">Separated link</a>
+                    </li>
+                    <li class="divider">
+                    </li>
+                    <li>
+                      <a href="#">One more separated link</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" />
+                </div> <button type="submit" class="btn btn-default">Submit</button>
+              </form>
+              <ul class="nav navbar-nav navbar-right">
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="#">Action</a>
+                    </li>
+                    <li>
+                      <a href="#">Another action</a>
+                    </li>
+                    <li>
+                      <a href="#">Something else here</a>
+                    </li>
+                    <li class="divider">
+                    </li>
+                    <li>
+                      <a href="#">Separated link</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            
+          </nav>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">
+                Panel title
+              </h3>
+            </div>
+            <div class="panel-body">
+              Panel content
+            </div>
+            <div class="panel-footer">
+              Panel footer
+            </div>
+          </div>
+          <div class="carousel slide" id="carousel-260043">
+            <ol class="carousel-indicators">
+              <li class="active" data-slide-to="0" data-target="#carousel-260043">
+              </li>
+              <li data-slide-to="1" data-target="#carousel-260043">
+              </li>
+              <li data-slide-to="2" data-target="#carousel-260043">
+              </li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="item active">
+                <img alt="" :src="require('@/assets/img/风景1.jpg')" />
+                <div class="carousel-caption">
+                  <h4>
+                    First Thumbnail label
+                  </h4>
+                  <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                  </p>
+                </div>
+              </div>
+              <div class="item">
+                <img alt="" :src="require('@/assets/img/风景1.jpg')" />
+                <div class="carousel-caption">
+                  <h4>
+                    Second Thumbnail label
+                  </h4>
+                  <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                  </p>
+                </div>
+              </div>
+              <div class="item">
+                <img alt="" :src="require('@/assets/img/风景1.jpg')" />
+                <div class="carousel-caption">
+                  <h4>
+                    Third Thumbnail label
+                  </h4>
+                  <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                  </p>
+                </div>
+              </div>
+            </div> <a class="left carousel-control" href="#carousel-260043" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-260043" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="thumbnail">
+                <img alt="300x200" :src="require('@/assets/img/风景1.jpg')" />
+                <div class="caption">
+                  <h3>
+                    Thumbnail label
+                  </h3>
+                  <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi port gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                  </p>
+                  <p>
+                    <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="thumbnail">
+                <img alt="300x200" :src="require('@/assets/img/风景1.jpg')" />
+                <div class="caption">
+                  <h3>
+                    Thumbnail label
+                  </h3>
+                  <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                  </p>
+                  <p>
+                    <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="thumbnail">
+                <img alt="300x200" :src="require('@/assets/img/风景1.jpg')" />
+                <div class="caption">
+                  <h3>
+                    Thumbnail label
+                  </h3>
+                  <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                  </p>
+                  <p>
+                    <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="collapse navbar-collapse" id="example-navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">iOS</a></li>
-          <li><a href="#">SVN</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              Java <b class="caret"></b>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="#">jmeter</a></li>
-              <li><a href="#">EJB</a></li>
-              <li><a href="#">Jasper Report</a></li>
-              <li class="divider"></li>
-              <li><a href="#">分离的链接</a></li>
-              <li class="divider"></li>
-              <li><a href="#">另一个分离的链接</a></li>
-            </ul>
-          </li>
-        </ul>
+      <div class="clearfix row">
+        <div class="col-md-4 column">
+          <h2>
+            Heading
+          </h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+          </p>
+          <p>
+            <a class="btn" href="#">View details »</a>
+          </p>
+        </div>
+        <div class="col-md-4 column">
+          <h2>
+            Heading
+          </h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+          </p>
+          <p>
+            <a class="btn" href="#">View details »</a>
+          </p>
+        </div>
+        <div class="col-md-4 column">
+          <h2>
+            Heading
+          </h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+          </p>
+          <p>
+            <a class="btn" href="#">View details »</a>
+          </p>
+        </div>
       </div>
-      </div>
-    </nav>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <button type="button" class="btn btn-default btn-lg">
