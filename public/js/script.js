ab82ea1f44db0ff61fcda93b9ec34576019d680c
Kinron <3323502972@qq.com>
2021-08-08 20:50:17 +0800

    添加雪花背景

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// See sources on GitHub:
+// https://github.com/mkalygin/snowflakes
+
+var snowConfig = {
+  color: [161 / 256, 197 / 256, 231 / 256],
+  count: 200,
+  opacity: 0.6,
+  density: 1 / 50
+};
