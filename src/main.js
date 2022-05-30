9f3b1727656fa742e3f4b08df95661702fe7826f
linjinrong <3323502972@qq.com>
2022-05-30 09:30:05 +0800

    fix: 问题修复

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'vue2-animate/dist/vue2-animate.min.css'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
